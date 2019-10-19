cb01cba082a32f538eb74ad138fc9c2ec7c3b29b
znznzn707 <znznzn707@163.com>
2019-10-19 16:05:20 +0800

    add LowerCaseInputStream code and update WeatherStationTest

--- src/test/java/com/github/znznzn707/WeatherStationTest.java
+++ src/test/java/com/github/znznzn707/WeatherStationTest.java
@@ -3,6 +3,11 @@ package com.github.znznzn707;
 import WeatherStation.*;
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class WeatherStationTest {
     @Test
     public void Test1() {
@@ -25,5 +30,23 @@ public class WeatherStationTest {
         weatherData2.setMeasurements(60,78,78.1f);
 
         System.out.println(weatherData2.countObservers());
+    }
+    @Test
+    public void TestInput() {
+        int c ;
+        try{
+            InputStream in =
+                    new LowerCaseInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("test.txt")
+                            )
+                    ) ;
+            while ((c = in.read()) >= 0) {
+                System.out.print((char)c);
+            }
+            in.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
